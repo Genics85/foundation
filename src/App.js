6bb1abd251d57f62f9f92193a6f700c19c93e1c3
unknown <eugeneamo85@gmail.com>
2023-03-06 11:40:09 +0000

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <span>Hello world</span>
+      <Button>Custom bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
