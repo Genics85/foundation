670b37820286a91654cb15724b0f083ad184d86e
unknown <eugeneamo85@gmail.com>
2023-03-06 12:49:58 +0000

    done with navigation bar layout

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,5 +1,19 @@
-import { Container, Row, Col } from "react-bootstrap";
-import { Facebook, Instagram, Linkedin, TelephoneFill, Twitter } from "react-bootstrap-icons";
+import {
+  Container,
+  Row,
+  Col,
+  Nav,
+  Offcanvas,
+  Navbar,
+  Button,
+} from "react-bootstrap";
+import {
+  Facebook,
+  Instagram,
+  Linkedin,
+  TelephoneFill,
+  Twitter,
+} from "react-bootstrap-icons";
 
 function Navigation() {
   return (
@@ -9,20 +23,23 @@ function Navigation() {
           <Row>
             <Col>
               <div className="number-mail text-light d-flex gap-4">
-                <span> <TelephoneFill/> +233-234567890</span>
+                <span>
+                  {" "}
+                  <TelephoneFill /> +233-234567890
+                </span>
                 <span> foundation@gmail.com</span>
               </div>
             </Col>
             <Col>
               <div className="socialmedia-handles d-flex gap-4 justify-content-end">
                 <a href="facebook.com">
-                  <Facebook className="text-light"/>
+                  <Facebook className="text-light" />
                 </a>
                 <a href="instagram.com">
                   <Instagram className="text-light" />
                 </a>
                 <a href="twitter.com">
-                  <Twitter className="text-light"/>
+                  <Twitter className="text-light" />
                 </a>
                 <a href="linkedin.com">
                   <Linkedin className="text-light" />
@@ -32,6 +49,34 @@ function Navigation() {
           </Row>
         </Container>
       </header>
+      {["md"].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+          <Container >
+            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">About</Nav.Link>
+                  <Nav.Link href="#action2">Blog</Nav.Link>
+                  <Nav.Link href="#action2">Contact</Nav.Link>
+                </Nav>
+                <Button className="rounded-pill text-light btn-secondary">Donate Now</Button>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
     </div>
   );
 }
